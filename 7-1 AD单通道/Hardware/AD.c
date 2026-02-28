c25e37bec6458c3ac330f04bfecae2856a9aa0aa
listenwind_wb <3105318748@qq.com>
2026-02-28 16:59:21 +0800

    2026.2.28

--- 7-1 AD单通道/Hardware/AD.c
+++ 7-1 AD单通道/Hardware/AD.c
@@ -5,7 +5,7 @@ void AD_Init(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	
+
 	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -20,7 +20,7 @@ void AD_Init(void)
 	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
 	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
 	ADC_InitStructure.ADC_NbrOfChannel = 1;
 	ADC_Init(ADC1, &ADC_InitStructure);
@@ -32,21 +32,13 @@ void AD_Init(void)
 	ADC_StartCalibration(ADC1);
 	while (ADC_GetCalibrationStatus(ADC1) == SET);
 	
-	
-	
-	
-	
-	
-	
-	
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 }
 
 uint16_t AD_GetValue(void)
 {
-	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
-	
-	
+	return ADC_GetConversionValue(ADC1);
+}
 
 
 
